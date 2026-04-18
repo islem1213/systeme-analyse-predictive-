c1338529fe0184df1c8963b91c86727b2b3820e5
islem <alimiislem99@gmail.com>
2026-04-18 10:45:00 +0100

    Fix: restore circular reference protection and sync all changes

--- src/main/java/com/example/demo/entity/Client.java
+++ src/main/java/com/example/demo/entity/Client.java
@@ -17,14 +17,43 @@ public class Client extends Utilisateur {
     private List<DemandePret> demandes;
 
     // Getters et Setters
-    public Double getRevenuMensuel() { return revenuMensuel; }
-    public void setRevenuMensuel(Double revenuMensuel) { this.revenuMensuel = revenuMensuel; }
-    public Double getChargesFixes() { return chargesFixes; }
-    public void setChargesFixes(Double chargesFixes) { this.chargesFixes = chargesFixes; }
-    public String getProfession() { return profession; }
-    public void setProfession(String profession) { this.profession = profession; }
-    public String getSituationFamiliale() { return situationFamiliale; }
-    public void setSituationFamiliale(String situationFamiliale) { this.situationFamiliale = situationFamiliale; }
-    public List<DemandePret> getDemandes() { return demandes; }
-    public void setDemandes(List<DemandePret> demandes) { this.demandes = demandes; }
+    public Double getRevenuMensuel() {
+        return revenuMensuel;
+    }
+
+    public void setRevenuMensuel(Double revenuMensuel) {
+        this.revenuMensuel = revenuMensuel;
+    }
+
+    public Double getChargesFixes() {
+        return chargesFixes;
+    }
+
+    public void setChargesFixes(Double chargesFixes) {
+        this.chargesFixes = chargesFixes;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public String getSituationFamiliale() {
+        return situationFamiliale;
+    }
+
+    public void setSituationFamiliale(String situationFamiliale) {
+        this.situationFamiliale = situationFamiliale;
+    }
+
+    public List<DemandePret> getDemandes() {
+        return demandes;
+    }
+
+    public void setDemandes(List<DemandePret> demandes) {
+        this.demandes = demandes;
+    }
 }
